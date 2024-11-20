299cda1c49e4341aec7b93e4d91f7eaf887ebac8
the-rooster <andrewmjculberson@gmail.com>
2024-11-19 17:04:54 -0700

    client logging

--- client/index.js
+++ client/index.js
@@ -16,17 +16,30 @@ function createWsResponsePromise(ws, requestId, timeout) {
         let timeout = setTimeout(() => {
             reject('timeout');
         }, timeout);
-
         ws.addEventListener('message', (event) => {
             let response = JSON.parse(event.data);
             if (response.requestId !== requestId) {
                 return;
             }
-
             clearTimeout(timeout);
             resolve(response);
         });
     });
+}
+
+const StegoAppLogger = {
+    info: function(...message) {
+        console.log('StegoApp: info', message)
+    },
+    error: function(...message) {
+        console.error('StegoApp: error', message)
+    },
+    warn: function(...message) {
+        console.warn('StegoApp: warn', message)
+    },
+    debug: function(...message) {
+        console.debug('StegoApp: debug', message)
+    }
 }
 
 export const StegoBaseApp = {
@@ -47,17 +60,17 @@ export const StegoBaseApp = {
         this.timeout = timeout
         this.debug = debug
 
-        this.debug && console.log('connecting to', `ws://${this.baseUrl}/ws`)
+        this.debug && StegoAppLogger.debug('connecting to', `ws://${this.baseUrl}/ws`)
 
         this.ws = new WebSocket(`ws:/${baseUrl}/ws`)
         this.ws.addEventListener('message', (event) => {
-            console.log('Message from server ', event.data)
+            this.debug && StegoAppLogger.debug('message', event.data)
         });
 
         return this;
     },
     get: function(path) {
-        this.debug && console.log('get', path)
+        this.debug && StegoAppLogger.debug('get', path)
         const requestId = generateRequestId()
         this.ws.send(formatRequest('get', requestId, {
             query: {
@@ -67,7 +80,7 @@ export const StegoBaseApp = {
         return createWsResponsePromise(this.ws, requestId, this.timeout);
     },
     list: function(path) {
-        this.debug && console.log('list', path)
+        this.debug && StegoAppLogger.debug('list', path)
         const requestId = generateRequestId()
         this.ws.send(formatRequest('list', requestId, {
             query: {
@@ -88,7 +101,7 @@ export const StegoBaseApp = {
         return createWsResponsePromise(this.ws, requestId, this.timeout);
     },
     update: function(path, value) {
-        this.debug && console.log('update', path, value)
+        this.debug && StegoAppLogger.debug('update', path, value)
         const requestId = generateRequestId()
         this.ws.send(formatRequest('update', requestId, {
             query: {
@@ -99,7 +112,7 @@ export const StegoBaseApp = {
         return createWsResponsePromise(this.ws, requestId, this.timeout);
     },
     delete: function(path) {
-        this.debug && console.log('delete', path)
+        this.debug && StegoAppLogger.debug('delete', path)
         const requestId = generateRequestId()
         this.ws.send(formatRequest('delete', requestId, {
             query: {
@@ -109,7 +122,7 @@ export const StegoBaseApp = {
         return createWsResponsePromise(this.ws, requestId, this.timeout);
     },
     subscribe: function(path) {
-        this.debug && console.log('subscribe', path)
+        this.debug && StegoAppLogger.debug('subscribe', path)
         // TODO: this
         this.ws.send(JSON.stringify({
             type: "subscribe",
@@ -119,17 +132,7 @@ export const StegoBaseApp = {
                     path: path
                 }
             }}))
-    },
-    unsubscribe: function(path) {
-        this.debug && console.log('unsubscribe', path)
-        // TODO: this
-        this.ws.send(JSON.stringify({
-            type: "unsubscribe",
-            requestId: Math.random().toString(36).substring(7),
-            payload: {
-                query: {
-                    path: path
-                },
-            }}))
+        // TODO: stream values back until unsubscribe
+        // TODO: return unsubscribe function
     }
 }
